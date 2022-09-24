f87b8450637078fd199e87670cb1e299f9b93289
boguirre <boguirre@gmail.com>
2022-09-24 00:59:37 -0500

    dashboard cambiado

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,28 +22,40 @@
         <h4 class="text-center">DASHBOARD</h4>
     </div>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active m"></li>
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="../src/assets/img/2.jpg" alt="First slide">
+                <img class="d-block w-100" src="../src/assets/img/1.jpeg" alt="First slide">
+                <div class="carousel-caption d-none d-sm-block">
+                    <h3>First slide</h3>
+                    <h5>Lorem ipsum dolor sit amet, dolore magna aliqua.</h5>
+                </div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="../src/assets/img/1.jpeg" alt="Second slide">
+                <img class="d-block w-100" src="../src/assets/img/3.jpeg" alt="Second slide">
+                <div class="carousel-caption d-none d-sm-block">
+                    <h3>Second slide</h3>
+                    <h5>Lorem ipsum dolor sit amet, dolore magna aliqua.</h5>
+                </div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="../src/assets/img/3.jpeg" alt="Third slide">
+                <img class="d-block w-100" src="../src/assets/img/2.jpg" alt="Third slide">
+                <div class="carousel-caption d-none d-sm-block">
+                    <h3>Third slide</h3>
+                    <h5>Lorem ipsum dolor sit amet, dolore magna aliqua.</h5>
+                </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </a>
