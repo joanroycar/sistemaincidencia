fb5c2c98eea325fda0491c7d3f231058da2c804a
boguirre <boguirre@gmail.com>
2022-09-24 00:26:43 -0500

    cambios peque

--- resources/views/incidence/index.blade.php
+++ resources/views/incidence/index.blade.php
@@ -131,7 +131,7 @@
                                             </svg>
 
                                         </a>
-                                        <a href="{{route('incidences.edit',$incidence)}}" class="bs-tooltip"
+                                        {{-- <a href="{{route('incidences.edit',$incidence)}}" class="bs-tooltip"
                                             data-bs-toggle="tooltip" data-bs-placement="top" title="Editar"
                                             data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg"
                                                 width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -139,7 +139,7 @@
                                                 stroke-linejoin="round" class="feather feather-edit-2 p-1 br-8 mb-1">
                                                 <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z">
                                                 </path>
-                                            </svg></a>
+                                            </svg></a> --}}
 
                                         <button type="submit" class="" style="background-color: none"><svg
                                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24"
--- resources/views/incidence/ssoma/index.blade.php
+++ resources/views/incidence/ssoma/index.blade.php
@@ -126,14 +126,14 @@
                                         </svg>
 
                                     </a>
-                                    <a href="{{route('incidente.edit',$incidence)}}" class="bs-tooltip"
+                                    {{-- <a href="{{route('incidente.edit',$incidence)}}" class="bs-tooltip"
                                         data-bs-toggle="tooltip" data-bs-placement="top" title="Ver Detaller"
                                         data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24"
                                             height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                                             class="feather feather-edit-2 p-1 br-8 mb-1">
                                             <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-                                        </svg></a>
+                                        </svg></a> --}}
                                     <a href="{{route('incidente.view',$incidence)}}" class="bs-tooltip"
                                         data-bs-toggle="tooltip" data-bs-placement="top" title="Ver Detaller"
                                         data-original-title="Edit">Recursos</a>
